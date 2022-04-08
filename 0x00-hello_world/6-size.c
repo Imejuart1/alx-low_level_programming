74cd41b448c0006b1f28feb86a9514621b09e2ec
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2022-04-08 15:02:48 -0700

    final

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
-*main prints the size of various type Return: 0 if exited properly, non-zero otherwise 
+*main - prints the size of various type
+*Return: 0 if exited properly, non-zero otherwise 
 */
 int main(void)
 {
