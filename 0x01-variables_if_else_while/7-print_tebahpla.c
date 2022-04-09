227b6eda2a7c60b7eaf68dbb98e6def7c9afdc1a
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2022-04-09 14:24:53 -0700

    next7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase but reverse order,
+ * followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+	{
+		for (ch = 'z'; ch >= 'a'; ch--)
+		{
+			putchar(ch);
+		}
+		putchar('\n');
+		return (0);
+	}
+}
