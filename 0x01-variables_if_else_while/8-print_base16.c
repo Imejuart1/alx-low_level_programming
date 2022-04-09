f0dae14a1662ef5dc21b2cba4edef5e978392a81
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2022-04-09 15:27:24 -0700

    nexxxt8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int ch;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch < 'g'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+} 
