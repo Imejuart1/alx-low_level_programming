68908c7ce6778ee17849593d9169e8a878432f5d
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2022-04-12 07:10:51 -0700

    loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
 #include "main.h"
 /** main - Print "_putchar" while using the function putchar
  * Return(0): success
  */
 int main(void)
 {
-	char * message = "_putchar\n";
-
-		int count = 0;
-	while (message[count] != 0)
-	{
-		count ++;
-	}
-	for(int i =0; i < count; ++i)
-		putchar(message[i]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return(0);
 }
