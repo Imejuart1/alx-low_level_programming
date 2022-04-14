e7833047e37333d870d085f4f2e3442103f71567
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2022-04-14 07:38:30 -0700

    cp

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
-
 /**
  * main - debugging
  *
  * Return: 0
- */int main(void)
+ */
+int main(void)
 {
-int i;
+	int i;
 
-i = 0;
-
-positive_or_negative(i);
-
-return (0);
+	i = 0;
+	positive_or_negative(i);
+	
+	return (0);
 }
